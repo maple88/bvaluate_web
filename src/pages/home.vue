8d254b31300ff93661d01ec31eb104cc94d1acce
ZhaoShaofeng <bem.zhao@foxmail.com>
2018-07-16 18:28:07 +0800

    home 热门行业

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,27 +1,233 @@
 <template>
   <div class="page">
-    <vheader/>
-    <div class="maintainer">
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
-      <p>我在这里</p>
+    <div class="home-header">
+      <div class="tophead">
+        <div class="container">
+          <p>APELINk</p>
+          <span>中/EN</span>
+        </div>
+      </div>
+      <div class="mainhead">
+        <nav class="navbar navbar-default">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><img src="../assets/logo-white.png"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">首页</a></li>
+                <li><a href="#">关注</a></li>
+                <li><a href="#">项目</a></li>
+                <li><a href="#">作者</a></li>
+                <li><a href="#">个人中心</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" >
+                    <div class="user"><img src="../assets/logo_brand.png"></div>
+                    <p>糖果数：1450</p>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">设置</a></li>
+                    <li><a href="#">退出</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+      </div>
     </div>
+    <div class="maintainer home-maintainer">
+      <div class="home-banner">
+        <img src="../assets/home/home-banner.jpg" class="bgimg">
+        <div class="bannerwds">
+          <img src="../assets/logo_brand.png">
+          <span>+</span>
+          APELINk
+        </div>
+      </div>
+      <div class="df-container container home-newslist">
+        <div class="section-head">
+          <div class="headtit">新闻头条</div>
+          <a href="#" class="more">浏览更多</a>
+        </div>
+        <div class="df-row row newslist">
+
+          <div class="df-col col-md-3 col-sm-4 col-xs-6">
+            <div class="item">
+              <a href="#">
+                <div class="img-box"><img src="../assets/news.jpg"></div>
+                <div class="info">
+                  <p class="tips">行业<span>2018年06月05日</span></p>
+                  <p class="tit">在你自己的手中采取事情：不放弃你的权力。</p>
+                  <p class="des">Lorem ipsum dolor sit amet，consectetur adipisicing elit，sed do eiusmod tempor incididunt ut labore et dolore magna aliqua。U en minim minim ven ven ven ven ven ven</p>
+                  <div class="bottom">
+                    <div class="left">
+                      <div class="userimg"><img src="../assets/logo_brand.png"></div>
+                      Sunny
+                    </div>
+                    <div class="right">阅读全文</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="df-col col-md-3 col-sm-4 col-xs-6">
+            <div class="item">
+              <a href="#">
+                <div class="img-box"><img src="../assets/news.jpg"></div>
+                <div class="info">
+                  <p class="tips">行业<span>2018年06月05日</span></p>
+                  <p class="tit">在你自己的手中采取事情：不放弃你的权力。</p>
+                  <p class="des">Lorem ipsum dolor sit amet，consectetur adipisicing elit，sed do eiusmod tempor incididunt ut labore et dolore magna aliqua。U en minim minim ven ven ven ven ven ven</p>
+                  <div class="bottom">
+                    <div class="left">
+                      <div class="userimg"><img src="../assets/logo_brand.png"></div>
+                      Sunny
+                    </div>
+                    <div class="right">阅读全文</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="df-col col-md-3 col-sm-4 col-xs-6">
+            <div class="item">
+              <a href="#">
+                <div class="img-box"><img src="../assets/news.jpg"></div>
+                <div class="info">
+                  <p class="tips">行业<span>2018年06月05日</span></p>
+                  <p class="tit">在你自己的手中采取事情：不放弃你的权力。</p>
+                  <p class="des">Lorem ipsum dolor sit amet，consectetur adipisicing elit，sed do eiusmod tempor incididunt ut labore et dolore magna aliqua。U en minim minim ven ven ven ven ven ven</p>
+                  <div class="bottom">
+                    <div class="left">
+                      <div class="userimg"><img src="../assets/logo_brand.png"></div>
+                      Sunny
+                    </div>
+                    <div class="right">阅读全文</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="df-col col-md-3 col-sm-4 col-xs-6">
+            <div class="item">
+              <a href="#">
+                <div class="img-box"><img src="../assets/news.jpg"></div>
+                <div class="info">
+                  <p class="tips">行业<span>2018年06月05日</span></p>
+                  <p class="tit">在你自己的手中采取事情：不放弃你的权力。</p>
+                  <p class="des">Lorem ipsum dolor sit amet，consectetur adipisicing elit，sed do eiusmod tempor incididunt ut labore et dolore magna aliqua。U en minim minim ven ven ven ven ven ven</p>
+                  <div class="bottom">
+                    <div class="left">
+                      <div class="userimg"><img src="../assets/logo_brand.png"></div>
+                      Sunny
+                    </div>
+                    <div class="right">阅读全文</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="df-col col-md-3 col-sm-4 col-xs-6">
+            <div class="item">
+              <a href="#">
+                <div class="img-box"><img src="../assets/news.jpg"></div>
+                <div class="info">
+                  <p class="tips">行业<span>2018年06月05日</span></p>
+                  <p class="tit">在你自己的手中采取事情：不放弃你的权力。</p>
+                  <p class="des">Lorem ipsum dolor sit amet，consectetur adipisicing elit，sed do eiusmod tempor incididunt ut labore et dolore magna aliqua。U en minim minim ven ven ven ven ven ven</p>
+                  <div class="bottom">
+                    <div class="left">
+                      <div class="userimg"><img src="../assets/logo_brand.png"></div>
+                      Sunny
+                    </div>
+                    <div class="right">阅读全文</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="df-container container hotindustry">
+        <div class="row">
+          <div class="section-head">
+            <div class="headtit">热门行业</div>
+            <a href="#" class="more">浏览更多</a>
+          </div>
+          <div class="inlist">
+            <ul>
+              <li class="on">
+                <img src="../assets/home/i1.jpg" class="inlistimg">
+                <div class="mask">
+                  <p>Datablockchain (PreICO)</p>
+                </div>
+              </li>
+              <li>
+                <img src="../assets/home/i2.jpg" class="inlistimg">
+                <div class="mask">
+                  <p>Datablockchain (PreICO)</p>
+                </div>
+              </li>
+              <li>
+                <img src="../assets/home/i3.jpg" class="inlistimg">
+                <div class="mask">
+                  <p>Datablockchain (PreICO)</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="intabcontent">
+            <div class="topban">
+              <div class="info">
+                <div class="toplist-item">
+                  <div class="left">
+                    <p class="tit">Datablockchain (PreICO) <i class="fa fa-heart"></i></p>
+                    <p class="smtit">Merging Big Data, AI And Blockchain</p>
+                    <p class="des">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibuset magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.</p>
+                  </div>
+                  <div class="right"><img src="../assets/home/ibing.jpg"></div>
+                </div>
+                <div class="mid">
+                  <p><span>代币</span>FSN</p>
+                  <p><span>价格</span>IETH=400FSN</p>
+                </div>
+                <div class="bottom">
+                  <ul>
+                    <li>folow us:</li>
+                    <li><a href="#"><img src="../assets/home/f1.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f2.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f3.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f4.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f5.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f6.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f8.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f9.png"></a></li>
+                    <li><a href="#"><img src="../assets/home/f10.png"></a></li>
+                  </ul>
+                  <div class="tips">评</div>
+                </div>
+              </div>
+              <img src="../assets/home/topban.jpg" class="topbanimg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="height: 1000px;"></div>
   </div>
 </template>
 
