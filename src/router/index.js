51b4f50b7cebe73127414295846bf8a328101fc1
MrFish <>
2018-12-27 15:51:28 +0800

    添加下载页面

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,7 @@ import list from '@/pages/list'
 import search from '@/pages/search'
 import newsList from '@/pages/newsList'
 import pdfShow from '@/pages/pdfShow'
+import download from '@/pages/download'
 
 Vue.use(Router);
 
@@ -115,6 +116,10 @@ export default new Router({
     {
       path: '/pdfShow',
       component: pdfShow
+    },
+    {
+      path: '/download',
+      component: download
     }
   ],
   scrollBehavior(to, from, savedPosition) {
